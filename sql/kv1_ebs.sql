8bc7cf517f36516194d51775647054787d06b5fa
Skywave <thakoch@gmail.com>
2012-09-21 18:14:19 +0200

    Adapt to new KV1 as supplied by EBS

--- sql/kv1_ebs.sql
+++ sql/kv1_ebs.sql
@@ -1,30 +1,3 @@
-alter table pool drop constraint pool_version_fkey;
-alter table link drop constraint link_pkey;
-alter table link add constraint link_pkey PRIMARY KEY ("version", "dataownercode", "userstopcodebegin", "userstopcodeend", "validfrom");
-alter table pool add constraint pool_dataownercode_fkey FOREIGN KEY (Version, DataOwnerCode, UserStopCodeBegin, UserStopCodeEnd, LinkValidFrom) 
-REFERENCES 
-link (Version, DataOwnerCode, UserStopCodeBegin, UserStopCodeEnd, ValidFrom);
-
-alter table pool drop constraint pool_pkey;
-alter table pool add constraint pool_pkey PRIMARY KEY (Version, DataOwnerCode, UserStopCodeBegin, UserStopCodeEnd, LinkValidFrom, PointDataOwnerCode, 
-PointCode);
-
-alter table link drop column transporttype;
-alter table pool drop column transporttype;
-
-alter table pool_delta drop constraint pool_delta_dataownercode_fkey;
-alter table link_delta drop constraint link_delta_pkey;
-alter table link_delta add constraint link_delta_pkey PRIMARY KEY ("dataownercode", "userstopcodebegin", "userstopcodeend", "validfrom");
-alter table pool_delta add constraint pool_dataownercode_fkey FOREIGN KEY (DataOwnerCode, UserStopCodeBegin, UserStopCodeEnd, LinkValidFrom) 
-REFERENCES 
-link_delta ( DataOwnerCode, UserStopCodeBegin, UserStopCodeEnd, ValidFrom);
-
-alter table pool_delta drop constraint pool_delta_pkey;
-alter table pool_delta add constraint pool_delta_pkey PRIMARY KEY (DataOwnerCode, UserStopCodeBegin, UserStopCodeEnd, LinkValidFrom, 
-PointDataOwnerCode, 
-PointCode);
-
-alter table link_delta drop column transporttype;
-alter table pool_delta drop column transporttype;
 alter table jopatili drop column productformulatype;
 alter table jopatili_delta drop column productformulatype;
+
