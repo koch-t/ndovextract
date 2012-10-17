0858f93b55572f2e13bbb73609bf5bbfef16b72a
Skywave <thakoch@gmail.com>
2012-10-17 21:59:24 +0200

    Fix typo to fix KML->KMZ conversion

--- bin/create-gtfs-arriva.sh
+++ bin/create-gtfs-arriva.sh
@@ -12,5 +12,5 @@ ln -s gtfs-kv1arriva-$DATE.zip ../gtfs/arriva/gtfs-kv1arriva-latest.zip
 #Validate using Google TransitFeed
 python transitfeed/feedvalidator.py ../gtfs/arriva/gtfs-kv1arriva-$DATE.zip -o ../gtfs/arriva/gtfs-kv1arriva-$DATE.html -l 50000 --error_types_ignore_list=ExpirationDate,FutureService
 python transitfeed/kmlwriter.py ../gtfs/arriva/gtfs-kv1arriva-$DATE.zip
-zip ../gtfs/arriva/gtfs-kv1arriva-$DATE.kmz ../gtfs/arriva/gtfs-kv1arriva-$DATE.kml
+zip -j ../gtfs/arriva/gtfs-kv1arriva-$DATE.kmz ../gtfs/arriva/gtfs-kv1arriva-$DATE.kml
 rm ../gtfs/arriva/gtfs-kv1arriva-$DATE.kml
