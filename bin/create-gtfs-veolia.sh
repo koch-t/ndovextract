528298b7944b4a058d5de73cede20cdb46daa441
Skywave <thakoch@gmail.com>
2012-11-16 11:25:40 +0100

    Fix path in Veolia KMZ generation

--- bin/create-gtfs-veolia.sh
+++ bin/create-gtfs-veolia.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DATE=$(date +'%Y%m%d')
-#wget ../kv1feeds/veolia  --accept=zip -q -P ../kv1feeds/veolia -nd -r http://kv1.openov.nl/veolia/ -l 1
+wget ../kv1feeds/veolia  --accept=zip -q -P ../kv1feeds/veolia -nd -r http://kv1.openov.nl/veolia/ -l 1
+
 python manager.py -d kv1vtn -f ../kv1feeds/veolia
 psql -d kv1vtn -f ../sql/gtfs-shapes-veolia.sql
 psql -d kv1vtn -f ../sql/gtfs-shapes-passtimes.sql
@@ -12,5 +13,5 @@ ln -s gtfs-kv1veolia-$DATE.zip ../gtfs/veolia/gtfs-kv1veolia-latest.zip
 #Validate using Google TransitFeed
 python transitfeed/feedvalidator.py ../gtfs/veolia/gtfs-kv1veolia-$DATE.zip -o ../gtfs/veolia/gtfs-kv1veolia-$DATE.html -l 50000 --error_types_ignore_list=ExpirationDate,FutureService
 python transitfeed/kmlwriter.py ../gtfs/veolia/gtfs-kv1veolia-$DATE.zip
-zip ../gtfs/veolia/gtfs-kv1veolia-$DATE.kmz ../gtfs/veolia/gtfs-kv1veolia-$DATE.kml
+zip -j ../gtfs/veolia/gtfs-kv1veolia-$DATE.kmz ../gtfs/veolia/gtfs-kv1veolia-$DATE.kml
 rm ../gtfs/veolia/gtfs-kv1veolia-$DATE.kml
